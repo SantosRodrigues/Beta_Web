fee0333d9ac2a6461d3404a13074c3c86d6e55e3
Higor <higor.rx@gmail.com>
2023-04-20 10:58:25 -0300

    refactor: remover testes de emprestimo e utilizacao de aba 4devs

--- src/main/java/org/example/webDriverAcoes/WebDriverAcoes.java
+++ src/main/java/org/example/webDriverAcoes/WebDriverAcoes.java
@@ -11,8 +11,6 @@ public class WebDriverAcoes {
 
     private static WebDriver driver;
     private static WebDriverWait wait;
-    private static WebDriver driver4Devs;
-    private static WebDriverWait wait4Devs;
 
     public void iniciarNavegador() {
         switch (getProp().getProperty("prop.navigator")) {
@@ -31,28 +29,10 @@ public class WebDriverAcoes {
         wait = new WebDriverWait(driver, 30);
     }
 
-    public void abrirPagina4devsCPF() {
-        if(getProp().getProperty("prop.navigator").equalsIgnoreCase("chrome")){
-            driver4Devs = new ChromeDriver();
-        }else if(getProp().getProperty("prop.navigator").equalsIgnoreCase("firefox")){
-            driver4Devs = new FirefoxDriver();
-        }
-        driver4Devs.get(getProp().getProperty("prop.url.4devs.cpf"));
-        driver4Devs.manage().window().maximize();
-        wait4Devs = new WebDriverWait(driver4Devs, 30);
-    }
-
     public static WebDriver getDriver() {
         return driver;
     }
     public static WebDriverWait getWait() {
         return wait;
     }
-    public static WebDriver getDriver4Devs() {
-        return driver4Devs;
-    }
-    public static WebDriverWait getWait4Devs() {
-        return wait4Devs;
-    }
-
 }
