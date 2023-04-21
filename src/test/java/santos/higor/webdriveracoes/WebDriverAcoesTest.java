bf7689e569d3c4afb6992a1401897e317d61d74a
Higor Santos <higor.rx@gmail.com>
2023-04-21 18:16:58 +0000

    Feature/teste unitario e integracao na pipeline + artefatos (!14)

---
+++ src/test/java/santos/higor/webdriveracoes/WebDriverAcoesTest.java
@@ -0,0 +1,12 @@
+package santos.higor.webdriveracoes;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WebDriverAcoesTest {
+
+    @Test
+    public void teste(){
+        Assert.assertEquals("a", "a");
+    }
+}
